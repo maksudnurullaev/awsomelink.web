95d6dcca95857734e8a23c27e3edfb498b5e5e2c
maksudnurullaev <maksud.nurullaev@gmail.com>
2015-06-12 07:34:26 +0000

    Add participants page to recipients part

--- lib/AWSomeLink/R.pm
+++ lib/AWSomeLink/R.pm
@@ -9,7 +9,8 @@ sub _check_access{
     my $c = shift;
     my $user_type = $c->session('user type');
     if( !$user_type || $user_type ne 'recipient' ){
-        $c->redirect_to('/') ;
+        my $prefix = Utils::trim $c->stash->{prefix};
+        $c->redirect_to("/$prefix/p/authorization") ;
         return(0);
     }
     return(1);
@@ -25,6 +26,22 @@ sub start {
     Utils::P::project_deploy($c,$db,$project_db_id) if $project_db_id ;
 }
 
+sub files {
+    my $c = shift;
+    return if ! _check_access($c);
+};
+
+sub participants{
+    my $c = shift ;
+    return if ! _check_access($c);
+
+    my $prefix = Utils::trim $c->stash->{prefix} ;
+    my $db = Db->new($c,$prefix) ;
+
+    my $project_db_id = Utils::P::get_project_db_id($db,$prefix) ;
+    Utils::P::project_deploy($c,$db,$project_db_id) if $project_db_id ;
+    Utils::P::project_deploy_($c,$db,'recipient','participants') ;
+};
 
 1;
 
